3a02c062dab28976c75c99c8f1976561873e3c7f
Ellie04162606 <1181140808@qq.com>
2020-08-29 14:31:34 +0800

    [PokerFaceGame][Keanu & Ellie] test: pass test should_return_white_wins_when_play_game_given_black_3H_3D_5S_9C_KD_white_3C_3H_3S_8C_AH

--- src/main/java/com/example/Demo.java
+++ src/main/java/com/example/Demo.java
@@ -36,7 +36,7 @@ public class Demo {
     return String.valueOf(key);
   }
 
-  private String getMaxKey(List<Integer> black,List<Integer> white){
+  private String getMaxKey(List<Integer> black, List<Integer> white) {
     int highCart = -1;
     String res = "Tie";
     for (int i = 1; i < black.size(); i++) {
@@ -45,45 +45,51 @@ public class Demo {
         return "Black" + " wins. - with high card: " + transToString(highCart);
       } else if (black.get(black.size() - i) < white.get(black.size() - i)) {
         highCart = white.get(white.size() - i);
-        return  "White"+ " wins. - with high card: " + transToString(highCart);
+        return "White" + " wins. - with high card: " + transToString(highCart);
       }
     }
     return res;
   }
 
+  private String describeOfPair(Map<Integer, List<String>> map, String winner) {
+    int maxLength = 0;
+    int maxNumber = 0;
+    for (Integer i : map.keySet()) {
+      if (map.get(i).size() > maxLength) {
+        maxLength = map.get(i).size();
+        maxNumber = i;
+      }
+    }
+    if (maxLength == 2) {
+      return winner + " wins. - with pair of " + transToString(maxNumber);
+    }
+    if (maxLength == 3) {
+      return winner + " wins. - with Three " + transToString(maxNumber);
+    }
+    return "Tie";
+  }
+
   public String playGame(String input) {
     String[] blackInput = input.substring(7, 22).split(" ");
     String[] whiteInput = input.substring(30).split(" ");
-    Map<Integer, List<String>> blackMap =transToMap(blackInput);
-    Map<Integer, List<String>> whiteMap =transToMap(whiteInput);
+    Map<Integer, List<String>> blackMap = transToMap(blackInput);
+    Map<Integer, List<String>> whiteMap = transToMap(whiteInput);
     Set<Integer> set = blackMap.keySet();
     List<Integer> black = new ArrayList<Integer>(set);
     Set<Integer> set2 = whiteMap.keySet();
     List<Integer> white = new ArrayList<Integer>(set2);
-    if(black.size()==white.size()){
-      return getMaxKey(black,white);
-    }else if(black.size()-white.size()==1){
-      for(Integer i:set2 ){
-        if(whiteMap.get(i).size()==2){
-          return "White" + " wins. - with pair of " + transToString(i);
-        }
-      }
-    }else{
-      for(Integer i:set2 ){
-        if(whiteMap.get(i).size()==2){
-          return "Black" + " wins. - with pair of " + transToString(i);
-        }
-      }
+    if (black.size() == white.size()) {
+      return getMaxKey(black, white);
     }
-    return "Tie";
+    return black.size() - white.size() > 0 ? describeOfPair(whiteMap, "White") : describeOfPair(blackMap, "Black");
   }
 
   private Map transToMap(String[] input) {
     Map<Integer, List<String>> map = new HashMap<Integer, List<String>>();
     for (int i = 0; i < input.length; i++) {
       List<String> value = new ArrayList<String>();
-      if(map.containsKey((transToInt(input[i].substring(0, 1))))){
-        value=map.get(transToInt(input[i].substring(0, 1)));
+      if (map.containsKey((transToInt(input[i].substring(0, 1))))) {
+        value = map.get(transToInt(input[i].substring(0, 1)));
       }
       value.add(input[i].substring(1));
       map.put(transToInt(input[i].substring(0, 1)), value);

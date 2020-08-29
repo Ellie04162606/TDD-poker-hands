1bbe535e42ab7845ff76571db836196961de9a5b
Ellie04162606 <1181140808@qq.com>
2020-08-29 17:08:34 +0800

    [PokerFaceGame][Keanu & Ellie] test: test should_return_white_wins_when_play_game_given_black_5H_3D_5S_9C_KD_white_3C_3H_4S_8C_AH

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -49,6 +49,18 @@ public class DemoTest {
   }
 
   @Test
+  public void should_return_white_wins_when_play_game_given_black_5H_3D_5S_9C_KD_white_3C_3H_4S_8C_AH(){
+    //given
+    String input = "Black: 5H 3D 5S 9C KD  White: 3C 3H 4S 8C AH";
+
+    //when
+    String result = demo.playGame(input);
+
+    //then
+    assertEquals("Black wins. - with pair of 5", result);
+  }
+
+  @Test
   public void should_return_white_wins_when_play_game_given_black_3H_3D_5S_9C_KD_white_3C_3H_3S_8C_AH(){
     //given
     String input = "Black: 3H 3D 5S 9C KD  White: 3C 3H 3S 8C AH";

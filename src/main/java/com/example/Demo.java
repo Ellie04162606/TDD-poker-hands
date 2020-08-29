96f7ac5e18f342fb958639b9677416d2c77e8b2c
Ellie04162606 <1181140808@qq.com>
2020-08-29 15:16:56 +0800

    [PokerFaceGame][Keanu & Ellie] test: pass test should_return_white_wins_when_play_game_given_black_3H_3D_5S_5C_KD_white_3C_3H_3S_8C_AH

--- src/main/java/com/example/Demo.java
+++ src/main/java/com/example/Demo.java
@@ -60,15 +60,50 @@ public class Demo {
         maxNumber.add(i);
       }
     }
-    if (maxLength == 2) {
-      if(map.size()==4)
-        return winner + " wins. - with pair of " + transToString(maxNumber.get(0));
-      return winner + " wins. - with two pairs of " + transToString(maxNumber.get(0))+"&"+transToString(maxNumber.get(1));
-    }
     if (maxLength == 3) {
       return winner + " wins. - with Three " + transToString(maxNumber.get(0));
     }
+    if (maxLength == 2) {
+      if (map.size() == 4) {
+        return winner + " wins. - with pair of " + transToString(maxNumber.get(0));
+      }
+      return winner + " wins. - with two pairs of " + transToString(maxNumber.get(0)) + "&" + transToString(
+          maxNumber.get(1));
+    }
     return "Tie";
+  }
+
+  private String whenLengthEqual(Map<Integer, List<String>> blackMap, Map<Integer, List<String>> whiteMap) {
+    Set<Integer> set = blackMap.keySet();
+    Set<Integer> set2 = whiteMap.keySet();
+    List<Integer> black = new ArrayList<Integer>(set);
+    List<Integer> white = new ArrayList<Integer>(set2);
+    if (whiteMap.size() == 5) {
+      return getMaxKey(black, white);
+    }
+    if (whiteMap.size() == 3) {
+      int maxLength = 0;
+      String winner = "";
+      List<Integer> maxNumber = new ArrayList<Integer>();
+      for (Integer i : whiteMap.keySet()) {
+        if (!(whiteMap.get(i).size() < maxLength)) {
+          maxLength = whiteMap.get(i).size();
+          maxNumber.add(i);
+          winner = "White";
+        }
+      }
+      for (Integer i : blackMap.keySet()) {
+        if (!(blackMap.get(i).size() < maxLength)) {
+          maxLength = blackMap.get(i).size();
+          maxNumber.add(i);
+          winner = "Black";
+        }
+      }
+      if (maxLength == 3) {
+        return winner + " wins. - with Three " + transToString(maxNumber.get(0));
+      }
+    }
+    return null;
   }
 
   public String playGame(String input) {
@@ -77,11 +112,11 @@ public class Demo {
     Map<Integer, List<String>> blackMap = transToMap(blackInput);
     Map<Integer, List<String>> whiteMap = transToMap(whiteInput);
     Set<Integer> set = blackMap.keySet();
-    List<Integer> black = new ArrayList<Integer>(set);
     Set<Integer> set2 = whiteMap.keySet();
+    List<Integer> black = new ArrayList<Integer>(set);
     List<Integer> white = new ArrayList<Integer>(set2);
     if (black.size() == white.size()) {
-      return getMaxKey(black, white);
+      return whenLengthEqual(blackMap, whiteMap);
     }
     return black.size() - white.size() > 0 ? describeOfPair(whiteMap, "White") : describeOfPair(blackMap, "Black");
   }
